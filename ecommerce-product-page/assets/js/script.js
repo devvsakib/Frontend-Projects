db90b07985468c26f83fbef957e43a50891c26d9
DevvSakib <devvsakib@gmail.com>
2022-08-25 14:22:42 +0600

    Quantity JS

--- ecommerce-product-page/assets/js/script.js
+++ ecommerce-product-page/assets/js/script.js
@@ -1,5 +1,22 @@
-const cartItems = document.getElementById('cart_items')
+const cartItems = document.getElementById('cart_items'),
+    prod_increase = document.getElementById('prod_increase')
+    prod_decrease = document.getElementById('prod_decrease'),
+prod_quanty = document.getElementById('prod_quanty')
 
 function showOff(params) {
     cartItems.classList.toggle("showoff");
 }
+let quanty = 0
+prod_increase.addEventListener('click', () => {
+    // quanty++
+    quanty++
+    prod_quanty.innerText = quanty
+})
+prod_decrease.addEventListener('click', () => {
+    // quanty++
+    quanty--
+    if (quanty < 0 ){
+        quanty = 0
+    }
+    prod_quanty.innerText = quanty
+})
