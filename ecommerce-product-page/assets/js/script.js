fd8ae20688f01f00e8a901787488f1726adeebab
DevvSakib <devvsakib@gmail.com>
2022-08-26 00:49:48 +0600

    All Dynamic| cart | Gallery

--- ecommerce-product-page/assets/js/script.js
+++ ecommerce-product-page/assets/js/script.js
@@ -1,7 +1,11 @@
 const cartItems = document.getElementById('cart_items'),
-    prod_increase = document.getElementById('prod_increase')
+    prod_increase = document.getElementById('prod_increase'),
     prod_decrease = document.getElementById('prod_decrease'),
-prod_quanty = document.getElementById('prod_quanty')
+    more_img = document.getElementById('more_img'),
+    prev_img = document.getElementById('prev_img'),
+    add_to_cart = document.getElementById('add_to_cart'),
+    for_prev_demo = document.querySelectorAll('.for_prev_demo'),
+    prod_quanty = document.getElementById('prod_quanty');
 
 function showOff(params) {
     cartItems.classList.toggle("showoff");
@@ -15,8 +19,62 @@ prod_increase.addEventListener('click', () => {
 prod_decrease.addEventListener('click', () => {
     // quanty++
     quanty--
-    if (quanty < 0 ){
+    if (quanty < 0) {
         quanty = 0
     }
     prod_quanty.innerText = quanty
+})
+
+///   Product Demo Images    ///
+for_prev_demo.forEach(element => {
+    element.addEventListener('click', () => {
+        prev_img.src = element.src
+        for_prev_demo.forEach(element => {
+            element.classList.remove('active')
+            event.target.classList.add('active')
+        })
+    })
+})
+
+
+// for_prev_demo.forEach(element => {
+//     element.addEventListener('click', (e) => {
+//         prev_img.src = element.src
+//         e.classList.remove('active')
+//         event.target.classList.add('active')
+//     })
+// })
+///  Cart Product Add Systems ///
+
+add_to_cart.addEventListener('click', () => {
+    let getNumberOfQuant = prod_quanty.innerText
+    getNumberOfQuant = Number.parseInt(getNumberOfQuant, 10)
+    actualPrice = parseFloat(125.00)
+    totalPrice = actualPrice * getNumberOfQuant
+    let cartHtml = `
+        <div class="cart_title">
+            <h2>Cart</h2>
+        </div>
+        <div class="items">
+        <div class="item_img">
+          <img src="./assets/images/image-product-1.jpg" alt="">
+        </div>
+        <div class="item_des">
+          <div class="item_title">
+            <h5>Fall Limited Edition Sneakers</h5>
+          </div>
+          <div class="item_quantity">
+            <span>$125.00 x ${getNumberOfQuant} <span class="" bold_price>$${totalPrice}.00</span></span>
+          </div>
+        </div>
+        <div class="item_delete">
+          <i class="fa-solid fa-trash"></i>
+        </div>
+        </div>
+        <div class="cart_btn">
+            <button>Checkout</button>
+        </div>
+    `
+    cartItems.innerHTML = ''
+    cartItems.innerHTML =cartHtml
 })
