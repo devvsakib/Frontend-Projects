ccba6b5dc830436922201391a4fc91b0024d6094
joe-sida <yohannestesfayejo@gmail.com>
2022-10-19 21:07:39 +0530

    added a simple JavaScript random quote generator

---
+++ quote generator/dist/script.js
@@ -0,0 +1,119 @@
+var colors = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857',
+  '#C6AEC7',
+'#D2B9D3',
+'#D8BFD8',
+'#DFD3E3',	
+'#E9CFEC',	
+'#FCDFFF',	
+'#EBDDE2',	
+'#E1D9D1',	
+'#E9E4D4',	
+'#EDE6D6',	
+'#FAF0DD',	
+'#F8F0E3',	
+'#F8F6F0',	
+'#F3E8EA',	
+'#FFF0F5',	
+'#FDEEF4',
+'#FFF9E3',	
+'#FDF5E6',	
+'#FAF0E6',	
+'#FFF5EE',	
+'#F9F6EE',
+'#FAF5EF',
+  '#FFFFF0',
+'#FFFFF4',
+'#FFFFF7',
+'#F5F5F5'
+];
+var currentQuote = '',
+  currentAuthor = '';
+
+function getQuotes() {
+  return $.ajax({
+    headers: {
+      Accept: 'application/json'
+    },
+    url:'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    success: function (jsonQuotes) {
+      if (typeof jsonQuotes === 'string') {
+        quotesData = JSON.parse(jsonQuotes);
+        console.log('quotesData');
+        console.log(quotesData);
+      }
+    }
+  });
+}
+
+function getRandomQuote() {
+  return quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
+  ];
+}
+
+function getQuote() {
+  let randomQuote = getRandomQuote();
+
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
+
+  $('#tweet-quote').attr(
+    'href',
+    'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+
+  $('#tumblr-quote').attr(
+    'href',
+    'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+      encodeURIComponent(currentAuthor) +
+      '&content=' +
+      encodeURIComponent(currentQuote) +
+      '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+  );
+
+  $('.quote-text').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#text').text(randomQuote.quote);
+  });
+
+  $('.quote-author').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#author').html(randomQuote.author);
+  });
+
+  var color = Math.floor(Math.random() * colors.length);
+  $('html body').animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color]
+    },
+    1000
+  );
+  $('.button-17').animate(
+    {
+      backgroundColor: colors[color]
+    },
+    1000
+  );
+}
+
+$(document).ready(function () {
+  getQuotes().then(() => {
+    getQuote();
+  });
+
+  $('#new-quote').on('click', getQuote);
+});
