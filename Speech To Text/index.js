37c19a77ab0b667ee61791a9f0ba23519d4bfb4f
Udit Kumar Gupta <68100794+udit9876gupta@users.noreply.github.com>
2022-10-19 19:01:32 +0530

    Update index.js

--- Speech To Text/index.js
+++ Speech To Text/index.js
@@ -1,17 +1,17 @@
-const btn = document.getElementById("btn");  
+// const btn = document.getElementById("btn");  
 const results = document.getElementById("result");  
 const speechRecognition = window.speechRecognition || window.webkitSpeechRecognition;  
 const recognition = new speechRecognition();  
 recognition.onstart = function(){  
-  console.log("you can speek now");  
+//   console.log("you can speek now");  
 }  
 recognition.onresult = function(event){  
-  var text = event.results[0][0].transcript;  
-  console.log(text);  
+  const text = event.results[0][0].transcript;  
+//   console.log(text);  
   document.getElementById("result").innerHTML = text;  
 }  
 function copyDivToClipboard() {  
-  let range = document.createRange();  
+  const range = document.createRange();  
   range.selectNode(document.getElementById("result"));  
   window.getSelection().removeAllRanges(); // clear current selection  
   window.getSelection().addRange(range); // to select text  
