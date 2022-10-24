fdddcf6c81b2acd7a96b22be8a0f3a2fc76dbda4
kmly <kemilyteixeira047@gmail.com>
2022-10-24 20:20:08 -0300

    feat: memory game project in React TS

---
+++ memory-game-react/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import { FooterPage } from '../../styled';
+
+export function Footer() {
+  return (
+    <FooterPage>
+      <ul>
+          <li>
+            <a href="https://github.com/kmlyteixeira"><img src="../images/github.png"></img></a>
+            <a href="https://www.linkedin.com/in/kemily-teixeira/"><img src="../images/linkedin.png"></img></a>
+          </li>
+        </ul>
+    </FooterPage>
+  );
+}
