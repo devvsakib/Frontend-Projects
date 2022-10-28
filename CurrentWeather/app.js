efe9ada1aa2817c6c8acac9ee647becb17c8eba7
Divyansh <92048020+Divi03@users.noreply.github.com>
2022-10-28 11:53:49 +0530

    Current Weather (#55)
    * Add files via upload
    
    * Update app.js
    
    Co-authored-by: DevvSakib <devvsakib@gmail.com>

---
+++ CurrentWeather/app.js
@@ -0,0 +1,70 @@
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+
+const weatherApi = {
+    key: "bab281d79e5f1e9755a68d754cc313e7",
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather", 
+}
+
+const searchInputBox = document.getElementById('input-box');
+
+// Event Listener Function on keypress
+searchInputBox.addEventListener('keypress', (event) => {
+    
+    if(event.keyCode == 13) {
+        getWeatherReport(searchInputBox.value);
+        document.querySelector('.weather-body').style.display = "block";
+    }
+
+});
+
+// Get Weather Report
+function getWeatherReport(city) {
+    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+    .then(weather => {
+        return weather.json();
+    }).then(showWeatherReport);
+}
+
+// Show Weather Report
+function showWeatherReport(weather){
+
+    let city = document.getElementById('city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let temperature = document.getElementById('temp');
+    temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+    let minMaxTemp = document.getElementById('min-max');
+    minMaxTemp.innerHTML = `${Math.floor(weather.main.temp_min)}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max) `;
+
+    let weatherType = document.getElementById('weather');
+    weatherType.innerText = `${weather.weather[0].main}`;
+
+    let date = document.getElementById('date');
+    let todayDate = new Date();
+    date.innerText = dateManage(todayDate);
+
+    let weather_iconEl = document.getElementById('weather-icon');
+    weather_iconEl.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png">
+    <div class="weather" id="weather">${weather.weather[0].main}</div>
+    `
+    let cityname = `${weather.name}` ;
+    document.body.style.background = "url('https://source.unsplash.com/1600x900/?" + cityname +"') no-repeat"
+    document.body.style.backgroundSize = "cover"  
+}
+
+// Date manage
+function dateManage(dateArg) {
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let year = dateArg.getFullYear();
+    let month = months[dateArg.getMonth()];
+    let date = dateArg.getDate();
+    let day = days[dateArg.getDay()];
+    
+    return `${date} ${month} (${day}), ${year}`;
+}
+
